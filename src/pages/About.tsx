f3ee4bc6706613b629fcb7362f700c97d9d6142e
whiteout15 <djwhite503@gmail.com>
2024-05-06 00:35:52 -0400

    Removed Colons

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -42,23 +42,23 @@ const About: React.FC = () => {
                 <h2 style={{textAlign: "center"}}>Pace University<br />CS389 Software Engineering</h2>
             </div>
             <div>
-                <h2 style={{fontWeight: "bold", textAlign: "center", textDecoration: "underline", fontStyle: "normal"}}>Created for:</h2>
+                <h2 style={{fontWeight: "bold", textAlign: "center", textDecoration: "underline", fontStyle: "normal"}}>Created for</h2>
                 <h2 style={{textAlign: "center"}}>Dr. Deep Parikh</h2>
                 <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
                     <img src="the_doc.jpg"></img>
                 </div>
             </div>
             <div>
-                <h2 style={{fontWeight: "bold", textAlign: "center", textDecoration: "underline", fontStyle: "normal"}}>Vision AI Team:</h2>
+                <h2 style={{fontWeight: "bold", textAlign: "center", textDecoration: "underline", fontStyle: "normal"}}>Vision AI Team</h2>
                 <h2 style={{textAlign: "center"}}>Sean Kahn<br />Daniel White<br />Cristian Bolanos<br />Jack Lasko</h2>
             </div>
             <div>
-                <h2 style={{fontWeight: "bold", textAlign: "center", textDecoration: "underline", fontStyle: "normal"}}>Acknowledgement:</h2>
+                <h2 style={{fontWeight: "bold", textAlign: "center", textDecoration: "underline", fontStyle: "normal"}}>Acknowledgement</h2>
                 <h2 style={{textAlign: "center", marginLeft: "2vh", marginRight: "2vh"}}> Thank you to Professor Thomas Schmidt for connecting us with Dr. Parikh and for working with us for the whole school year.</h2>
             </div>
             <div>
-                <h2 style={{fontWeight: "bold", textAlign: "center", textDecoration: "underline", fontStyle: "normal"}}>Contact:</h2>
-                <h2 style={{textAlign: "center", marginLeft: "2vh", marginRight: "2vh"}}>Dr. Parikh: <br /></h2>
+                {/* <h2 style={{fontWeight: "bold", textAlign: "center", textDecoration: "underline", fontStyle: "normal"}}>Contact:</h2>
+                <h2 style={{textAlign: "center", marginLeft: "2vh", marginRight: "2vh"}}>Dr. Parikh: <br /></h2> */}
             </div>
             
           </div>
